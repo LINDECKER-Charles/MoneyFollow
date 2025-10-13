f7998bad5b0720e0b44d1d70d0a483dc3f18c770
Shirou-Emiya2420 <charles.lindecker@outlook.fr>
2025-10-13 17:03:08 +0200

    feat(ux) : ajout d'un rejex pour le mot de passe et le mail dans la création de compte

--- frontend/src/app/components/auth/register/register.component.ts
+++ frontend/src/app/components/auth/register/register.component.ts
@@ -3,41 +3,42 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { UserRequestService } from 'src/app/services/request/user-request.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [CommonModule, FormsModule, RouterModule],
-  templateUrl: './register.component.html',
+templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
 
-  email = '';
-  username = '';
-  password = '';
-  passwordConfirm = '';
-  passwordMismatch = false;
-  showPassword = false;
-  showConfirm = false;
-  errorMessage = '';
+  public email: string = '';
+  public username: string = '';
+  public password: string = '';
+  public passwordConfirm: string = '';
+
+  public showPassword: boolean = false;
+  public showConfirm: boolean = false;
+  
+  public errorMessage: string = '';
   
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private UserRequestService: UserRequestService) {}
  
-  togglePasswordVisibility() {
+  public togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
   }
-  toggleConfirmVisibility() {
+  public toggleConfirmVisibility() {
     this.showConfirm = !this.showConfirm;
   }
 
-  onSubmit(): void {
-    if (this.password !== this.passwordConfirm) {
-      this.passwordMismatch = true;
+  public onSubmit(): void {
+    if (this.notTakenEmail() || this.notSamePassword() || this.notValidPassword() || this.notValidEmail()) {
       return;
     }
-    this.passwordMismatch = false;
+
     this.authService.register(this.email, this.password, this.username).subscribe({
       next: () => {
         console.log('✅ Inscription réussie');
@@ -48,5 +49,50 @@ export class RegisterComponent {
         this.errorMessage = 'Erreur lors de l\'inscription : email peut-être déjà utilisé ou serveur injoignable';
       }
     });
+  }
+
+  private notSamePassword(): boolean{
+    if(this.password !== this.passwordConfirm){
+      this.errorMessage = "Les mots de passe ne correspondent pas.";
+      return true;
+    }
+    return false;
+  }
+
+  private notValidPassword(): boolean{
+    const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+    if(!strongPasswordRegex.test(this.password)){
+      this.errorMessage = 'Le mot de passe doit contenir au minimum 8 caractères, avec une majuscule, une minuscule, un chiffre et un symbole.';
+      return true;
+    }
+    return false;
+  }
+
+  private notValidEmail(): boolean{
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+    if(!emailRegex.test(this.email)){
+      this.errorMessage = "L'adresse e-mail n'est pas valide.";
+      return true;
+    }
+    return false;
+  }
+
+  private notTakenEmail(): boolean{
+    this.UserRequestService.getEmailAvailability(this.email).subscribe({
+      next: (reponse) => {
+        console.log(reponse);
+        if(reponse.available !== true){
+          this.errorMessage = "L'adresse e-mail est déjà prise.";
+          return true;
+        }else{
+          return false;
+        }
+      },
+      error: (err) => {
+          console.log(err);
+          return false;
+      },
+    })
+    return false;
   }
 }
